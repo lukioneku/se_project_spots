84b650cd04e1e819e78904271afda3522004197d
Yaniel Delgado <lukioneku@gmail.com>
2026-04-26 16:37:24 -0400

    Stage 6 again :'{{{{

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,28 @@
 const initialCards = [
   {
     name: 'Val Thorens',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg'}
-    {
-    name: 'Restaurant terrace',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg'}
-    {
-    name: 'An outdoor cafe',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg'}
-    {
-    name: 'A very long bridge, over the forest and through the trees',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg'}
-    {
-    name: 'Tunnel with morning light',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg'}
-    {
-    name: 'Mountain house',
-    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg'}
-    
+    link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg'
+   },
+  {
+  name: 'Restaurant terrace',
+  link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg'
+   },
+  {
+  name: 'An outdoor cafe',
+  link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg'
+   },
+  {
+  name: 'A very long bridge, over the forest and through the trees',
+  link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg'
+},
+  {
+  name: 'Tunnel with morning light',
+  link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg'
+},
+  {
+  name: 'Mountain house',
+  link: 'https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg'
+}
 ];
 
 const editProfileBtn = document.querySelector('.profile__edit-btn');
@@ -93,7 +98,7 @@ addCardFormElement.addEventListener('submit', handleAddCardSubmit);
 editProfileForm.addEventListener('submit', handleEditProfileFormSubmit);
 newPostForm.addEventListener('submit', handleAddCardSubmit);
 
-initialCards.forEach(function(item)) {
+initialCards.forEach(function(card) {
   console.log('Name:', card.name);
   console.log('Link:', card.link);
-}
+});
